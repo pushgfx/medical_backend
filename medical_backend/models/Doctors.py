e32cad9c0c043e6657247a3711341bcceb2c9371
pushgfx <jon@pushgfx.com>
2020-02-29 16:22:23 -0600

    updates

--- medical_backend/models/Doctors.py
+++ medical_backend/models/Doctors.py
@@ -19,30 +19,51 @@ class Doctor:
 
 
 		date_arr = []
-		for d in range(30):
+		for d in range(1,31): # We don't allow same day booking online
 			future_date = current_date + timedelta(days=d)
-			timeslots = ["avail","avail","avail","avail","avail","avail","avail","avail"]
-			date_struct = {
-				"datetime": datetime.strftime(future_date, "%m/%d/%Y"),
-				"timeslots": timeslots
-			}
-			if future_date.isoweekday() < 6:
+			if future_date.isoweekday() < 7:
+				timeslots = ["avail","avail","avail","avail","avail","avail","avail","avail"]
+				date_struct = {
+					"datetime": datetime.strftime(future_date, "%m/%d/%Y"),
+					"timeslots": timeslots,
+					"office_id": 0
+				}
 				date_arr.append(date_struct)
-
 
 		# MARK BOOKED DATES/TIMESLOTS
 
-		# add a query
+		# Get a dictionary of all appointments for this doctor
 		sql = "SELECT appt_start_time, estimated_end_time FROM appointments WHERE `doctor_id`=%s"
 		params = (doctor_id)
 		cur.execute(sql, params)
-		# This is a dictionary of all appointments for this doctor_id
 		appointments = cur.fetchall()
-		for appointment in appointments:
-			if appointment['appt_start_time'].day > current_date.day:
-				for date in date_arr:
-					if date["datetime"].hour == appointment['appt_start_time'].hour:
-						slot = appointment['appt_start_time'].hour - 8
-						date["timeslots"][slot - 1] = "booked"
 
-		return dates
+		# Get a dictionary of all availability for this doctor
+		sql = "SELECT * FROM doctor_office_availability WHERE `doctor_id`=%s"
+		params = (doctor_id)
+		cur.execute(sql, params)
+		schedule = cur.fetchall()
+
+		# Gather unavailable timeslots from schedule dictionary
+		uTimes = []
+		for s in schedule:
+			if s['is_available'] == "N":
+				uTimes.append(s)
+
+		# Filter out the taken dates
+		for date in date_arr:
+			for appointment in appointments:
+				if appointment['appt_start_time'].day == date['datetime'].day:
+					slot = appointment['appt_start_time'].hour - 8
+					date["timeslots"][slot - 1] = "na"
+			for uTime in uTimes:
+				if uTime['day_of_week'] == date.isoweekday():
+					sBeg = uTime['start_time'].hour - 8
+					sEnd = uTime['end_time'].hour - 8
+					n = sEnd - sBeg
+					for x in range(n):
+						sl = sBeg + x
+						date['timeslots'][sl] = "na"
+						date['office_id'] = 
+
+		return date_arr
