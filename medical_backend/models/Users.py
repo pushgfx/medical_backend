81c87d53c27cdaeddd5cfddd94f5f46ff6371a6e
KeithDinh <keithdinhcs@gmail.com>
2020-02-29 19:31:53 -0600

    .

--- medical_backend/models/Users.py
+++ medical_backend/models/Users.py
@@ -19,10 +19,17 @@ class User:
 		cur.execute(sql, params)
 		result = cur.fetchone()
 		password_hash = result['password']
-		return checkpw(str.encode(str(password)), password_hash)
+		print("password_hash before",password_hash)
+		password_hash = password_hash.encode('utf-8')
+		print("password_hash after",password_hash)
+
+		print("password",password)
+		print("password str" ,str(password))
+		print("password str str",str.encode(str(password)))
+		return checkpw(str.encode(password), password_hash)
 
 	def add_user(self, email, password, role_id, user_id):
-		hashed = hashpw(str.encode(str(password)), gensalt(14))
+		hashed = hashpw(str.encode(password), gensalt(14))
 		sql = "INSERT INTO `users` (`id`, `email`, `password`, `role_id`, `user_role_id`) VALUES (NULL, %s, %s, %s, %s)"
 		params = (email,hashed,role_id,user_id)
 		cur.execute(sql, params)
