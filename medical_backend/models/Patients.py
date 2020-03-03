765349e101b8db76d9f879294727d96e08ca43ae
Y NGUYEN <thieny1991@yahoo.com>
2020-03-02 21:17:50 -0600

    .

--- medical_backend/models/Patients.py
+++ medical_backend/models/Patients.py
@@ -22,11 +22,14 @@ class Patient(User):
         req_marital = request.json.get("marital", None)
         req_race = request.json.get("race", None)
         req_email = request.json.get("email", None)
-        sql = "INSERT INTO `patients` (`patient_id`, `first_name`, `middle_initial`, `last_name`, `street_1`, `city`, `state`, `zipcode`, `phone`, `date_of_birth`, `gender`, `marital_status`, `race`, `email`, `primary_doctor`) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NULL)"
+        sql = "INSERT INTO `patients` (`patient_id`, `first_name`, `middle_initial`, `last_name`, `street_1`, `city`, " \
+              "`state`, `zipcode`, `phone`, `date_of_birth`, `gender`, `marital_status`, `race`, `email`, " \
+              "`primary_doctor`) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NULL) "
         params = (
-        str(req_first_name), str(req_middle_i), str(req_last_name), str(req_street_1), str(req_city), str(req_state),
-        str(req_zipcode), str(req_phone), str(req_dob), str(req_gender), str(req_marital), str(req_race),
-        str(req_email))
+            str(req_first_name), str(req_middle_i), str(req_last_name), str(req_street_1), str(req_city),
+            str(req_state),
+            str(req_zipcode), str(req_phone), str(req_dob), str(req_gender), str(req_marital), str(req_race),
+            str(req_email))
         cur.execute(sql, params)
         cur.execute("SELECT `patient_id` FROM `patients` ORDER BY `patient_id` DESC LIMIT 1")
         result = cur.fetchone()
@@ -35,11 +38,13 @@ class Patient(User):
         return uid
 
     def update_patient(self, r, patient_id):
-        sql = "UPDATE patients SET first_name=%s, middle_initial=%s, last_name=%s, street_1=%s, city=%s, state=%s, zipcode=%s, phone=%s, date_of_birth=%s, gender=%s, marital_status=%s, race=%s, email=%s WHERE patient_id=%s"
+        sql = "UPDATE patients SET first_name=%s, middle_initial=%s, last_name=%s, street_1=%s, city=%s, state=%s, " \
+              "zipcode=%s, phone=%s, date_of_birth=%s, gender=%s, marital_status=%s, race=%s, email=%s WHERE " \
+              "patient_id=%s "
         params = (
-        str(r.form['first_name']), str(r.form['middle_initial']), str(r.form['last_name']), str(r.form['street_1']),
-        str(r.form['city']), str(r.form['state']), str(r.form['zipcode']), str(r.form['phone']), str(r.form['dob']),
-        str(r.form['gender']), str(r.form['marital_status']), str(r.form['race']), str(r.form['email']), patient_id)
+            str(r.form['first_name']), str(r.form['middle_initial']), str(r.form['last_name']), str(r.form['street_1']),
+            str(r.form['city']), str(r.form['state']), str(r.form['zipcode']), str(r.form['phone']), str(r.form['dob']),
+            str(r.form['gender']), str(r.form['marital_status']), str(r.form['race']), str(r.form['email']), patient_id)
         cur.execute(sql, params)
 
     def get_patient_dict(self, patient_id):
@@ -64,7 +69,8 @@ class Patient(User):
         return profile
 
     def get_patient_appt_hist(self, patient_id):
-        sql = "SELECT appointments.appt_id,appointments.appt_start_time,appointments.appt_status,appointments.booking_date," \
+        sql = "SELECT appointments.appt_id,appointments.appt_start_time,appointments.appt_status," \
+              "appointments.booking_date," \
               "appointments.booking_method,doctors.first_name,doctors.last_name,offices.office_name " \
               "FROM appointments,doctors,offices " \
               "WHERE appointments.patient_id=%s " \
@@ -75,14 +81,14 @@ class Patient(User):
         appointments = cur.fetchall()
         return appointments
 
-    def get_patient_prescriptions(self,patient_id):
+    def get_patient_prescriptions(self, patient_id):
         sql = "SELECT D.first_name,D.last_name,MED.medication_name,MED_FORM.dose_form_name," \
-            "PRESC.dosage,PRESC.indication,PRESC.date_prescribed " \
-            "FROM doctors as D, medications as MED, medication_dose_forms as MED_FORM, prescribed_medications as PRESC" \
-            "WHERE PRESC.patient_id=%s" \
-            "AND PRESC.doctor_id=D.doctor_id " \
-            "AND MED.medication_id=PRESC.medication_id " \
-            "AND MED_FORM.dose_form_id=PRESC.dose_form_id"
+              "PRESC.dosage,PRESC.indication,PRESC.date_prescribed " \
+              "FROM doctors as D, medications as MED, medication_dose_forms as MED_FORM, prescribed_medications as PRESC" \
+              "WHERE PRESC.patient_id=%s" \
+              "AND PRESC.doctor_id=D.doctor_id " \
+              "AND MED.medication_id=PRESC.medication_id " \
+              "AND MED_FORM.dose_form_id=PRESC.dose_form_id"
         params = (patient_id)
         cur.execute(sql, params)
         prescriptions = cur.fetchall()
