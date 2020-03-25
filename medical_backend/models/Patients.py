bb1843da4a83d03ef221e51704b5419c506ed155
thieny1991 <thieny1991@yahoo.com>
2020-03-25 15:09:10 -0500

    return role_id change patient dob

--- medical_backend/models/Patients.py
+++ medical_backend/models/Patients.py
@@ -1,57 +1,108 @@
 from bcrypt import checkpw, gensalt, hashpw
 from ..database import Database
 from ..models import User
+import datetime
 
 db = Database()
 cur = db.cur
 
+
 class Patient(User):
 
-	def add_patient(self, request):
-		req_first_name = request.json.get("firstName", None)
-		req_middle_i = request.json.get("middleInit", None)
-		req_last_name = request.json.get("lastName", None)
-		req_street_1 = request.json.get("street", None)
-		req_city = request.json.get("city", None)
-		req_state = request.json.get("state", None)
-		req_zipcode = request.json.get("zipcode", None)
-		req_phone = request.json.get("phone", None)
-		req_dob = request.json.get("dob", None)
-		req_gender = request.json.get("gender", None)
-		req_marital = request.json.get("marital", None)
-		req_race = request.json.get("race", None)
-		req_email = request.json.get("email", None)
-		sql = "INSERT INTO `patients` (`patient_id`, `first_name`, `middle_initial`, `last_name`, `street_1`, `city`, `state`, `zipcode`, `phone`, `date_of_birth`, `gender`, `marital_status`, `race`, `email`, `primary_doctor`) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NULL)"
-		params = (str(req_first_name),str(req_middle_i),str(req_last_name),str(req_street_1),str(req_city),str(req_state),str(req_zipcode),str(req_phone),str(req_dob),str(req_gender),str(req_marital),str(req_race),str(req_email))
-		cur.execute(sql, params)
-		cur.execute("SELECT `patient_id` FROM `patients` ORDER BY `patient_id` DESC LIMIT 1")
-		result = cur.fetchone()
-		uid = result['patient_id']
-		self.add_user(req_email, request.json.get("password", None), 2, uid)
-		return uid
+    def add_patient(self, request):
+        req_first_name = request.json.get("firstName", None)
+        req_middle_i = request.json.get("middleInit", None)
+        req_last_name = request.json.get("lastName", None)
+        req_street_1 = request.json.get("street", None)
+        req_city = request.json.get("city", None)
+        req_state = request.json.get("state", None)
+        req_zipcode = request.json.get("zipcode", None)
+        req_phone = request.json.get("phone", None)
+        req_dob = request.json.get("dob", None)
+        req_gender = request.json.get("gender", None)
+        req_marital = request.json.get("marital", None)
+        req_race = request.json.get("race", None)
+        req_email = request.json.get("email", None)
+        sql = "INSERT INTO `patients` (`patient_id`, `first_name`, `middle_initial`, `last_name`, `street_1`, `city`, " \
+              "`state`, `zipcode`, `phone`, `date_of_birth`, `gender`, `marital_status`, `race`, `email`, " \
+              "`primary_doctor`) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NULL) "
+        params = (
+            str(req_first_name), str(req_middle_i), str(req_last_name), str(req_street_1), str(req_city),
+            str(req_state),
+            str(req_zipcode), str(req_phone), str(req_dob), str(req_gender), str(req_marital), str(req_race),
+            str(req_email))
+        cur.execute(sql, params)
+        cur.execute("SELECT `patient_id` FROM `patients` ORDER BY `patient_id` DESC LIMIT 1")
+        result = cur.fetchone()
+        uid = result['patient_id']
+        self.add_user(req_email, request.json.get("password", None), 2, uid)
+        return uid
 
-	def update_patient(self, r, patient_id):
-		sql = "UPDATE patients SET first_name=%s, middle_initial=%s, last_name=%s, street_1=%s, city=%s, state=%s, zipcode=%s, phone=%s, date_of_birth=%s, gender=%s, marital_status=%s, race=%s, email=%s WHERE patient_id=%s"
-		params = (str(r.form['first_name']),str(r.form['middle_initial']),str(r.form['last_name']),str(r.form['street_1']),str(r.form['city']),str(r.form['state']),str(r.form['zipcode']),str(r.form['phone']),str(r.form['dob']),str(r.form['gender']),str(r.form['marital_status']),str(r.form['race']),str(r.form['email']),patient_id)
-		cur.execute(sql, params)
+    def update_patient(self, r, patient_id):
+        sql = "UPDATE patients SET first_name=%s, middle_initial=%s, last_name=%s, street_1=%s, city=%s, state=%s, " \
+              "zipcode=%s, phone=%s, date_of_birth=%s, gender=%s, marital_status=%s, race=%s, email=%s WHERE " \
+              "patient_id=%s "
+        params = (
+            str(r.form['first_name']), str(r.form['middle_initial']), str(r.form['last_name']), str(r.form['street_1']),
+            str(r.form['city']), str(r.form['state']), str(r.form['zipcode']), str(r.form['phone']), str(r.form['dob']),
+            str(r.form['gender']), str(r.form['marital_status']), str(r.form['race']), str(r.form['email']), patient_id)
+        cur.execute(sql, params)
 
-	def get_patient_dict(self, patient_id):
-		sql = "SELECT * FROM `patients` WHERE patient_id=%s"
-		params = (patient_id,)
-		cur.execute(sql, params)
-		result = cur.fetchone()
-		profile = {
-			"firstName": result['first_name'],
-			"middleInit": result['middle_initial'],
-			"lastName": result['last_name'],
-			"street": result['street_1'],
-			"city": result['city'],
-			"state": result['state'],
-			"zipcode": result['zipcode'],
-			"phone": result['phone'],
-			"dob": result['date_of_birth'],
-			"gender": result['gender'],
-			"marital": result['marital_status'],
-			"race": result['race']
-		}
-		return profile
+    def get_patient_dict(self, patient_id):
+        sql = "SELECT * FROM `patients` WHERE patient_id=%s"
+        params = (patient_id,)
+        cur.execute(sql, params)
+        result = cur.fetchone()
+        profile = {
+            "patient_id": result['patient_id'], #medical record number
+            "firstName": result['first_name'],
+            "middleInit": result['middle_initial'],
+            "lastName": result['last_name'],
+            "street": result['street_1'],
+            "city": result['city'],
+            "state": result['state'],
+            "zipcode": result['zipcode'],
+            "phone": result['phone'],
+            "dob": str(datetime.datetime.strptime(str(result['date_of_birth']), '%Y-%m-%d').date()),
+            "gender": result['gender'],
+            "marital": result['marital_status'],
+            "race": result['race']
+        }
+        print(profile['dob'])
+        return profile
+
+    def get_patient_appt_hist(self, patient_id):
+        sql = "SELECT appointments.appt_id, appointments.appt_start_time, appointments.appt_status," \
+              "appointments.booking_date," \
+              "appointments.booking_method,doctors.first_name,doctors.last_name,offices.office_name " \
+              "FROM appointments,doctors,offices " \
+              "WHERE appointments.patient_id=%s " \
+              " AND appointments.doctor_id=doctors.doctor_id " \
+              " AND appointments.office_id=offices.office_id"
+        params = (patient_id)
+        cur.execute(sql, params)
+        appointments = cur.fetchall()
+        return appointments
+
+    def get_patient_prescriptions(self, patient_id):
+        sql = "SELECT PRESC.appt_id, D.first_name,D.last_name,MED.medication_name,MED_FORM.dose_form_name, " \
+              "PRESC.dosage,PRESC.indication,PRESC.date_prescribed " \
+              "FROM doctors as D, medications as MED, medication_dose_forms as MED_FORM, prescribed_medications as PRESC " \
+              "WHERE PRESC.patient_id=%s " \
+              "AND PRESC.doctor_id=D.doctor_id " \
+              "AND MED.medication_id=PRESC.medication_id " \
+              "AND MED_FORM.dose_form_id=PRESC.dose_form_id"
+        params = (patient_id)
+        cur.execute(sql, params)
+        prescriptions = cur.fetchall()
+        return prescriptions
+
+    def get_patient_medical_records(self,patient_id):
+        sql = "SELECT MR.appt_id,D.first_name,D.last_name,MR.actual_start_time,MR.actual_end_time " \
+              "FROM medical_records as MR, doctors as D " \
+              "WHERE MR.patient_id=%s " \
+              "AND MR.doctor_id = D.doctor_id"
+        params = (patient_id)
+        cur.execute(sql, params)
+        medical_records = cur.fetchall()
+        return medical_records
