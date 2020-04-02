0676fc88ee1f5f9d22091c93e9030d8e71be4600
thieny1991 <thieny1991@yahoo.com>
2020-04-02 12:37:08 -0500

    create endpoint, queries for doctors's appointments

--- medical_backend/controllers/doctor_controller.py
+++ medical_backend/controllers/doctor_controller.py
@@ -40,15 +40,14 @@ def get_doctor_route():
 	profile = doctor.get_doctor_dict(doctor_id)
 	doctor_patient = doctor.get_doctor_patient(doctor_id)
 	patient_appointments = doctor.get_doctor_all_appointment(doctor_id)
-	# TODO: I comment these 6 lines of code out, in case this is what you want to handle multiple requests
-	# today_appointments=doctor.get_today_appointments_by_doctor(doctor_id)
-	# future_appointments=doctor.get_future_appts_by_doctor(doctor_id)
-	# past_appointments=doctor.get_past_appts_by_doctor(doctor_id)
+	today_appointments=doctor.get_today_appointments_by_doctor(doctor_id)
+	future_appointments=doctor.get_future_appts_by_doctor(doctor_id)
+	past_appointments=doctor.get_past_appts_by_doctor(doctor_id)
 	if profile:
 		response, code = {"doctors": profile, "patients": doctor_patient, "appointments": patient_appointments}, 200
-		# response, code = {"doctors": profile, "patients": doctor_patient, "appointments": patient_appointments,
-		# 					"todayAppointments":today_appointments,"futureAppointments":future_appointments,
-		# 				  	"pastAppointments":past_appointments}, 200
+		response, code = {"doctors": profile, "patients": doctor_patient, "appointments": patient_appointments,
+							"todayAppointments":today_appointments,"futureAppointments":future_appointments,
+						  	"pastAppointments":past_appointments}, 200
 	else:
 		response, code = {"msg": "Bad doctor id"}, 400
 	# print(response)
