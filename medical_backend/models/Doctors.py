9fb5360cd4fd3804d29ff947bf10d8b27fd985a2
pushgfx <jon@pushgfx.com>
2020-04-11 19:56:21 -0500

    update

--- medical_backend/models/Doctors.py
+++ medical_backend/models/Doctors.py
@@ -262,7 +262,7 @@ class Doctor:
 
         return True
 
-    def add_patient_prescription(self, request):
+    def add_patient_prescription(self, request,doctor_id):
         appt_id = request.json.get("apptId", None)
         patient_id = request.json.get("patientId", None)
         medication_id = request.json.get("medicationId", None)
@@ -276,50 +276,36 @@ class Doctor:
 
         return True
 
-	def add_patient_prescription(self, request,doctor_id):
-		appt_id = request.json.get("apptId", None)
-		patient_id = request.json.get("patientId", None)
-		medication_id = request.json.get("medicationId", None)
-		dose_form_id = request.json.get("doseFormId", None)
-		dosage = request.json.get("dosage", None)
-		date_prescribed = request.json.get("datePrescribed", None)
-		sql = "INSERT INTO `prescribed_medications` (appt_id,doctor_id,patient_id,medication_id,dose_form_id,dosage,date_prescribed) " \
-			  "VALUES (%s,%s,%s,%s,%s,%s,%s);"
-		params = (str(appt_id), str(doctor_id), str(patient_id), str(medication_id), str(dose_form_id), str(dosage), str(date_prescribed))
-		db.run_query(sql, params)
+    def add_doctor(self, request):
+        req_first_name = request.json.get("firstName", None)
+        req_middle_i = request.json.get("middleInit", None)
+        req_last_name = request.json.get("lastName", None)
+        req_street_1 = request.json.get("street", None)
+        req_city = request.json.get("city", None)
+        req_state = request.json.get("state", None)
+        req_zipcode = request.json.get("zipcode", None)
+        req_phone = request.json.get("phone", None)
+        req_dob = request.json.get("dob", None)
+        req_gender = request.json.get("gender", None)
+        req_specialistId = request.json.get("specialistId", None)
+        req_race = request.json.get("race", None)
+        req_email = request.json.get("email", None)
+        req_image = request.json.get("image", None)
+        sql = "INSERT INTO `doctors` (`doctor_id`, `first_name`, `middle_initial`, `last_name`,`phone`, `specialist_id`,`gender`,`email`, `race`,`date_of_birth`, `street_1`, `city`, `state`, `zipcode`, 'image' ) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s) "
+        params = (
+            str(req_first_name), str(req_middle_i), str(req_last_name),str(req_phone),str(req_specialistId),
+            str(req_gender),str(req_email), str(req_race), str(req_dob), str(req_street_1), str(req_city),
+            str(req_state),str(req_zipcode),   str(req_image))
+        db.run_query(sql, params)
+        result = db.run_query("SELECT `doctor_id` FROM `doctors` ORDER BY `doctor_id` DESC LIMIT 1", ())
+        uid = result[0]['doctor_id']
+        self.add_user(req_email, request.json.get("password", None), 3, uid)
+        return uid
+    
+    def get_specializations(self):
+        # Get a dictionary of all the doctors (names, id's)
+        sql = "SELECT * FROM specializations"
+        params = ()
+        specialization = db.run_query(sql, params)
 
-		return True
-
-	def add_doctor(self, request):
-		req_first_name = request.json.get("firstName", None)
-		req_middle_i = request.json.get("middleInit", None)
-		req_last_name = request.json.get("lastName", None)
-		req_street_1 = request.json.get("street", None)
-		req_city = request.json.get("city", None)
-		req_state = request.json.get("state", None)
-		req_zipcode = request.json.get("zipcode", None)
-		req_phone = request.json.get("phone", None)
-		req_dob = request.json.get("dob", None)
-		req_gender = request.json.get("gender", None)
-		req_specialistId = request.json.get("specialistId", None)
-		req_race = request.json.get("race", None)
-		req_email = request.json.get("email", None)
-		req_image = request.json.get("image", None)
-		sql = "INSERT INTO `doctors` (`doctor_id`, `first_name`, `middle_initial`, `last_name`,`phone`, `specialist_id`,`gender`,`email`, `race`,`date_of_birth`, `street_1`, `city`, `state`, `zipcode`, 'image' ) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s) "
-		params = (
-			str(req_first_name), str(req_middle_i), str(req_last_name),str(req_phone),str(req_specialistId),
-			str(req_gender),str(req_email), str(req_race), str(req_dob), str(req_street_1), str(req_city),
-			str(req_state),str(req_zipcode),   str(req_image))
-		db.run_query(sql, params)
-		result = db.run_query("SELECT `doctor_id` FROM `doctors` ORDER BY `doctor_id` DESC LIMIT 1", ())
-		uid = result[0]['doctor_id']
-		self.add_user(req_email, request.json.get("password", None), 3, uid)
-		return uid
-	
-	def get_specializations(self):
-		# Get a dictionary of all the doctors (names, id's)
-		sql = "SELECT * FROM specializations"
-		params = ()
-		specialization = db.run_query(sql, params)
-
-		return specialization
+        return specialization
--- medical_backend/models/Offices.py
+++ medical_backend/models/Offices.py
@@ -4,32 +4,32 @@ db = Database()
 
 class Office:
 
-	def get_all_offices(self): 
-		sql = "SELECT * FROM offices" 
-		params = ()
-		offices = db.run_query(sql, params)
+    def get_all_offices(self): 
+        sql = "SELECT * FROM offices" 
+        params = ()
+        offices = db.run_query(sql, params)
 
-		return offices
+        return offices
 
-	def get_offices_by_doctor(self, doctor_id):
-		sql = "SELECT * FROM offices, doctor_office_affiliations WHERE doctor_office_affiliations.doctor_id=%s AND doctor_office_affiliations.office_id=offices.office_id"
-		params = (doctor_id)
-		offices = db.run_query(sql, params)
-		
-		return offices
+    def get_offices_by_doctor(self, doctor_id):
+        sql = "SELECT * FROM offices, doctor_office_affiliations WHERE doctor_office_affiliations.doctor_id=%s AND doctor_office_affiliations.office_id=offices.office_id"
+        params = (doctor_id)
+        offices = db.run_query(sql, params)
+        
+        return offices
 
-	def update_office(self, request):
-		payload = request.get_json()['payload']
-		office_id = payload['oid']
-		office_name = payload['oname']
-		office_street = payload['address']
-		office_city = payload['city']
-		office_state = payload['state']
-		office_zipcode = payload['zipcode']
-		office_phone = payload['phone']
+    def update_office(self, request):
+        office_id = request.json.get('office_id')
+        office_name = request.json.get('office_name')
+        office_street = request.json.get('street_1')
+        office_city = request.json.get('city')
+        office_state = request.json.get('state')
+        office_zipcode = request.json.get('zipcode')
+        office_phone = request.json.get('phone')
 
-		sql = """UPDATE offices SET office_name=%s, street_1=%s, city=%s, state=%s, zipcode=%s, phone=%s WHERE office_id=%s """
-		params = (str(office_name), str(office_street), str(office_city), str(office_state),str(office_zipcode), str(office_phone),office_id)
-		db.run_query(sql, params)
-		return True
-	
+        sql = """UPDATE offices SET office_name=%s, street_1=%s, city=%s, state=%s, zipcode=%s, phone=%s WHERE office_id=%s """
+        params = (str(office_name), str(office_street), str(office_city), str(office_state),str(office_zipcode), str(office_phone),office_id)
+        db.run_query(sql, params)
+        
+        return True
+    
