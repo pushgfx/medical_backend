374f6f871ccb44a12bf46a642a39c93c94664188
thieny1991 <thieny1991@yahoo.com>
2020-04-20 17:20:18 -0500

    update query for doctor doctor approval, future appts

--- medical_backend/models/Doctors.py
+++ medical_backend/models/Doctors.py
@@ -238,6 +238,7 @@ class Doctor(User):
             WHERE DATE(appointments.appt_start_time) > %s
             AND appointments.doctor_id=%s AND offices.office_id=appointments.office_id
             AND appointments.patient_id=patients.patient_id
+            AND (appointments.appt_status="pending")
             ORDER BY appointments.appt_start_time ASC"""
         params=(current_date,doctor_id)
         result = db.run_query(sql,params)
@@ -247,7 +248,7 @@ class Doctor(User):
         status="need approval"
         sql="""SELECT appointments.appt_id,appointments.patient_id, CONCAT(patients.first_name," ",patients.middle_initial, " ", patients.last_name) AS patient,
                         CONCAT(doctors.first_name," ",doctors.middle_initial, " ", doctors.last_name) AS doctor,
-                        offices.office_name AS office, appointments.appt_status, appointments.reason_for_visit
+                        offices.office_name AS office, appointments.appt_status, appointments.appt_start_time,appointments.reason_for_visit
                         FROM `appointments`,`offices`,`patients`, `doctors`
                         WHERE appointments.referring_doctor_id=%s
                         AND offices.office_id=appointments.office_id
